b8678b1eba850e470d99f036c2927d2e7fd40aa0
ruoyu0088 <ruoyu0088@gmail.com>
2025-03-16 17:07:29 +0900

    add cython-intro

--- scipybook/helper/cython.py
+++ scipybook/helper/cython.py
@@ -1,7 +1,26 @@
+import re
+from bs4 import BeautifulSoup
 from distutils.command.build import build
 from distutils.command.build_ext import build_ext
 from Cython.Build.IpythonMagic import CythonMagics
 
+def clean_html(html):
+    soup = BeautifulSoup(html, "html.parser")
+    styles = soup.head.find_all("style") if soup.head else []
+    body_content = soup.body.encode_contents() if soup.body else b""
+    cleaned_html = "".join(str(style) for style in styles) + body_content.decode()
+    return cleaned_html
+
+def clean_annotated_html(html):
+    r = re.compile('<p>Raw output: <a href="(.*)">(.*)</a>')
+    html = '\n'.join(l for l in html.splitlines() if not r.match(l))
+    html = clean_html(html)
+    html = html.replace("body.cython", "div.cython")
+    html = html.replace("pre {", "div.pre {")
+    # print(html)
+    return html
+    
+CythonMagics.clean_annotated_html = staticmethod(clean_annotated_html)
 
 if not hasattr(CythonMagics, '_get_build_extension_original'):
     CythonMagics._get_build_extension_original = CythonMagics._get_build_extension
