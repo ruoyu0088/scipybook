895c8c8d7bd938b639093774041fe81fc4e3e2f0
ruoyu0088 <ruoyu0088@gmail.com>
2025-04-08 20:55:11 +0900

    add pyvista-dataset

--- scipybook/helper/html.py
+++ scipybook/helper/html.py
@@ -2,7 +2,7 @@ import html
 
 def iframe(html_text, height="500px", width="500px"):
     html_code = html.escape(html_text)
-    iframe = f'<iframe srcdoc="{html_code}" height="{height}" width="{width}" sandbox="allow-scripts allow-same-origin" scrolling="no"></iframe>'
+    iframe = f'    <iframe srcdoc="{html_code}" height="{height}" width="{width}" sandbox="allow-scripts allow-same-origin" scrolling="no"></iframe>    '
     return iframe
 
 def html_table(rows, columns_name, title=None):
@@ -14,3 +14,14 @@ def html_table(rows, columns_name, title=None):
         table = f'{title}\n\n{table}'
 
     display_html(HTML(table))
+
+
+def latex_table(rows, columns_names, latex_columns):
+    import polars as pl
+    from great_tables import GT
+    df = pl.DataFrame(rows, schema=columns_names, orient="row")
+
+    def as_math(text):
+        return rf'<div class="math">\[{text}\]</div>'
+        
+    return GT(df).fmt(columns=latex_columns, fns=as_math)
--- scipybook/helper/pyvista.py
+++ scipybook/helper/pyvista.py
@@ -1,8 +1,29 @@
+import zlib
+import base64
+import zipfile
+import io
+from pathlib import Path
+from IPython.display import display_html, HTML
 import pyvista as pv
+from .html import iframe
+
+
+def export_to_small_html(plotter):
+    zip_data = plotter.export_vtksz(filename=None)
+    zf = zipfile.ZipFile(io.BytesIO(zip_data))
+    with zf.open('index.json') as f:
+        json_data = f.read()
+        compressed = zlib.compress(json_data)
+        b64_str = base64.b64encode(compressed).decode()
+
+    templete_file = Path(__file__).parent / 'vtk_scene.html'
+    with open(templete_file, encoding='utf-8') as f:
+        html = f.read().replace("%json_data%", b64_str)
+
+    return html
+
 
 def plotter_to_iframe(plotter, width=500, height=500, return_html=False):
-    from IPython.display import display_html
-    from .html import iframe
     
     if isinstance(width, int):
         width = f"{width}px"
@@ -10,9 +31,21 @@ def plotter_to_iframe(plotter, width=500, height=500, return_html=False):
     if isinstance(height, int):
         height = f"{height}px"
 
-    html = plotter.export_html(None).getvalue()
+    html = export_to_small_html(plotter)
+
     iframe_str = iframe(html, height=f"{height}px", width=f"{width}px")
     if return_html:
-        return iframe_str
+        return " " * 100 + iframe_str # disable iframe warning
     else:
-        display_html(iframe_str, raw=True)
+        # display_html(iframe_str, raw=True)
+        return HTML(iframe_str)
+
+
+def plot_point_indices(plotter, ds, scale, color='orange', index_name='vtkOriginalPointIds'):
+    for point, normal, i in zip(ds.points, ds.point_normals, ds.point_data[index_name]):
+        x, y, z = point
+        nx, ny, nz = normal * scale
+
+        text = str(int(i))
+        text_3d = pv.Text3D(text, width=scale*0.5 * len(text), depth=scale*0.2, height=scale, center=(x + nx, y + ny, z + nz))
+        plotter.add_mesh(text_3d, color=color)        
